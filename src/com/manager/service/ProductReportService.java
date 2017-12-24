8889aebbef72732a7c526c6e23f968a7fb663086
wenshantang <wenshan.tang@sao.so>
2017-12-24 19:39:31 +0800

    20171224

--- src/com/manager/service/ProductReportService.java
+++
@@ -1,6 +0,0 @@
-package com.manager.service;
-import cn.aresoft.common.service.BaseService;
-
-import com.manager.model.ProductReport;
-public interface ProductReportService extends BaseService<ProductReport>{
-}
---
+++ src/com/manager/service/RecruitService.java
@@ -0,0 +1,8 @@
+package com.manager.service;
+import com.manager.model.Recruit;
+
+import cn.aresoft.common.service.BaseService;
+
+public interface RecruitService extends BaseService<Recruit>{
+
+}
